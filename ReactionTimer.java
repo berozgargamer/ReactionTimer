3b0366226641431498a444d21af3be806d897149
berozgargamer <priyadarshan.dayal@gmail.com>
2020-02-06 04:12:24 +0530

    Add files via upload

---
+++ ReactionTimer.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ReactionTimer {
+
+    
+    public static void main(String[] args) throws InterruptedException {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("How fast can you Add ?");
+        
+        Thread.sleep(1000);
+        
+        double r1=Math.random()*100;
+        double r2=Math.random()*100;
+        
+        int rI1 = (int)r1;
+        int rI2 = (int)r2;
+        
+        System.out.println(rI1+"+"+rI2);
+        long startTime = System.currentTimeMillis();
+        
+        int sum = s.nextInt();
+        long endTime = System.currentTimeMillis();
+        
+        long reactionTime = endTime-startTime;
+        
+        System.out.print("Reaction time is "+reactionTime+" ms ");
+        if(sum!=(rI1+rI2)) System.out.println("but the answer is wrong !");
+        
+        
+    }
+    
+}
